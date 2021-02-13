f3b230dbb28a6e954d48b7cd291b18aa95cf91f4
Matt Stucky <mnstucky@gmail.com>
2021-02-12 22:10:21 -0600

    Fix bug in serving static files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-app.use(express.static(`${__dirname}/public`));
+app.use("/public", express.static("public"));
 
 app.use(bodyParser.urlencoded({extended: false}));
 
