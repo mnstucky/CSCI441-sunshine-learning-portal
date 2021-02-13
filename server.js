1a97cbccc038c6af04c082c1d3601f39fb29a21f
Matt Stucky <mnstucky@gmail.com>
2021-02-12 21:19:21 -0600

    Add routes for basic sitemap

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+app.use(express.static(`${__dirname}/public`));
+
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.get("/", (req, res) => {
-    res.send("Welcome to the future Sunshine Learning Portal.");
+    res.sendFile(`${__dirname}/views/index.html`);
+})
+
+app.get("/contact/", (req, res) => {
+    res.sendFile(`${__dirname}/views/contact.html`);
+})
+
+app.get("/discussions/", (req, res) => {
+    res.sendFile(`${__dirname}/views/discussions.html`);
+})
+
+app.get("/leaders/", (req, res) => {
+    res.sendFile(`${__dirname}/views/leaders.html`);
+})
+
+app.get("/learning/", (req, res) => {
+    res.sendFile(`${__dirname}/views/learning.html`);
+})
+
+app.get("/profile/", (req, res) => {
+    res.sendFile(`${__dirname}/views/profile.html`);
 })
 
 app.listen(port, () => 
