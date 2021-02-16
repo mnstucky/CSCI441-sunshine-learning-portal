4e4bd20eed42765a6602134534573c6d35867e0c
Matt Stucky <mnstucky@gmail.com>
2021-02-16 12:32:44 -0600

    Refactor using EJS

--- server.js
+++ server.js
@@ -3,33 +3,42 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-app.use("/public", express.static("public"));
+// Apply middleware
 
+app.use("/public", express.static("public"));
 app.use(bodyParser.urlencoded({extended: false}));
 
+// Set templating engine to EJS
+
+app.set('view engine', 'ejs');
+
+// Define routes
+
 app.get("/", (req, res) => {
-    res.sendFile(`${__dirname}/views/index.html`);
+    res.render(`${__dirname}/views/index`);
 })
 
 app.get("/contact/", (req, res) => {
-    res.sendFile(`${__dirname}/views/contact.html`);
+    res.render(`${__dirname}/views/contact`);
 })
 
 app.get("/discussions/", (req, res) => {
-    res.sendFile(`${__dirname}/views/discussions.html`);
+    res.render(`${__dirname}/views/discussions`);
 })
 
 app.get("/leaders/", (req, res) => {
-    res.sendFile(`${__dirname}/views/leaders.html`);
+    res.render(`${__dirname}/views/leaders`);
 })
 
 app.get("/learning/", (req, res) => {
-    res.sendFile(`${__dirname}/views/learning.html`);
+    res.render(`${__dirname}/views/learning`);
 })
 
 app.get("/profile/", (req, res) => {
-    res.sendFile(`${__dirname}/views/profile.html`);
+    res.render(`${__dirname}/views/profile`);
 })
+
+// Start server
 
 app.listen(port, () => 
     console.log(`App listening on port ${port}.`),
