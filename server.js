3d4d1373cf02550b943f97a0054e508d67c6a2c2
Matt Stucky <mnstucky@gmail.com>
2021-02-23 13:02:10 -0600

    Add database connection

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
+// Load basic routing services
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+const port = process.env.PORT || 3000;
+
+// Load local environmental variables
+require('dotenv').config();
+
+// Load services for user authentication
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const bcrypt = require("bcrypt");
-const port = process.env.PORT || 3000;
+
+// Load database services
+const { Pool, Client } = require('pg');
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({
+    connectionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 // Apply middleware
 
@@ -17,6 +34,15 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.set('view engine', 'ejs');
 
 // Define routes
+
+app.get("/db/", (req, res) => {
+    pool.query('SELECT * FROM logins', (err, data) => {
+        console.log(err);
+        console.log(data);
+        res.send(JSON.stringify(data.rows));
+        pool.end();
+    })
+})
 
 app.get("/", (req, res) => {
     res.render(`${__dirname}/views/index`);
