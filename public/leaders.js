983cda45e528f62b315679d66db1d29608595532
Matt Stucky <mnstucky@gmail.com>
2021-03-22 21:15:29 -0500

    Add to example

--- public/leaders.js
+++ public/leaders.js
@@ -7,5 +7,8 @@ document.addEventListener("readystatechange", e => {
 async function loadData() {
     const data = await fetch("/api?action=gettop10&track=4");
     // Anything you do with the data has to happen after you fetch the data using async/await
-    console.log(await data.json());
+    const formatted = await data.json();
+    const headline = document.querySelector('h2');
+    headline.textContent = formatted[0].studentid;
 }
+
