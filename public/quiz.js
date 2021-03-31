b639b31e36f7a1af90f969fc63e985da46f1a856
Matt Stucky <mnstucky@gmail.com>
2021-03-31 14:58:07 -0500

    Finish testing functionality for tracks starting with pretest

--- public/quiz.js
+++ public/quiz.js
@@ -12,32 +12,6 @@ document.addEventListener("readystatechange", (e) => {
   }
 });
 
-function getChoiceName(index) {
-  switch (index) {
-    case 0:
-      return "choicea";
-    case 1:
-      return "choiceb";
-    case 2:
-      return "choicec";
-    case 3:
-      return "choiced";
-  }
-}
-
-function getChoiceLetter(index) {
-  switch (index) {
-    case 0:
-      return "A";
-    case 1:
-      return "B";
-    case 2:
-      return "C";
-    case 3:
-      return "D";
-  }
-}
-
 async function startTrack(trackId, trackName) {
   // When the user starts a track, hide extra information
   hideDefaultDisplay();
@@ -58,21 +32,250 @@ async function startTrack(trackId, trackName) {
   if (results.pretestscore === 0) {
     takePretest(trackId);
   }
-  const canSkip = await fetch(`/api?action=canskip&track=${trackId}`);
-  if (!canSkip) {
-    displayMaterials(trackId);
-  } else {
-  }
 }
 
-function displayMaterials(trackId) {
+async function takePretest(trackId) {
+  const subHeader = document.querySelector("h2");
+  subHeader.textContent = "Pretest";
+  const myQuestions = await getRandomQuestions(trackId);
+  // myQuestions takes the form of an array of objects, such as:
+  // { answer: "C"
+  // choicea: "Cara ordered 7 pizza for her birthday party. Her parents ate 1/2 of a pizza before the party. How much pizza is left for the party?"
+  // choiceb: "Walt has 7 hamsters. Each hamster weighs 1/2 kilogram. What is the total weight of the hamsters?"
+  // choicec: "Jenae has 1/2 kilogram of trail mix. She splits her trail mix evenly between 7 friends. How much trail mix will each friend get?"
+  // choiced: "Jack ordered 7 cupcakes for his class. His parents ate 1/2 of a cupcakes before the school. How many cupcakes are left for the class?"
+  // question: "Which problem can we solve with 1/2 ÷7?"
+  // questionid: 12
+  // trackid: 4 }
+  displayQuestions(myQuestions);
+  const rawlearningMaterials = await fetch(
+    `/api/?action=getlearningmaterials&track=${trackId}`
+  );
+  let learningMaterials = await rawlearningMaterials.json();
+  learningMaterials = learningMaterials[0];
+  const submitButton = document.querySelector(".submitButton");
+  let correctAnswers = 0;
+  submitButton.onclick = async (event) => {
+    submitButton.disabled = true;
+    correctAnswers = gradeTest(myQuestions);
+    displayResults(correctAnswers);
+    const dataToPut = {
+      trackid: trackId,
+      test: "pretestscore",
+      score: correctAnswers,
+    };
+    const putResponse = await fetch("/api/results/", {
+      credentials: "same-origin",
+      method: "PUT",
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+      body: JSON.stringify(dataToPut),
+    });
+    const nextButton = document.querySelector(".nextButton");
+    nextButton.disabled = false;
+    nextButton.onclick = (event) => {
+      subHeader.textContent = "Learning Materials";
+      displayFirstMaterials(learningMaterials, trackId);
+    };
+  };
+}
+
+function gradeTest(myQuestions) {
+  let numCorrect = 0;
+  let currentQuestion = 0;
+  const allAnswerSets = document.querySelectorAll(".answers");
+  for (const answerSet of allAnswerSets) {
+    const correctAnswerLetter = myQuestions[currentQuestion].answer;
+    for (const possibleAnswer of answerSet.children) {
+      const radioButton = possibleAnswer.children[0];
+      const possibleAnswerLetter = radioButton.id;
+      if (correctAnswerLetter === possibleAnswerLetter) {
+        if (radioButton.checked) {
+          numCorrect += 1;
+          possibleAnswer.style.color = "lightgreen";
+        } else {
+          possibleAnswer.style.color = "red";
+        }
+      }
+    }
+    currentQuestion += 1;
+  }
+  return numCorrect;
+}
+
+async function displayFirstMaterials(learningMaterials, trackId) {
   clearScreen();
-  const quizContainer = document.getElementById("quiz");
-  quizContainer.innerHTML =
-    '<iframe width="560" height="315" src="https://youtu.be/bIRwzhhBzEU"> </iframe>';
+  const nextButton = document.querySelector(".nextButton");
+  nextButton.disabled = true;
+  const myQuestion = await getRandomQuestion(trackId);
+  const materialsContainer = document.getElementById("materials");
+  materialsContainer.innerHTML = learningMaterials.video1;
+  displayQuestion(myQuestion);
+  const submitButton = document.querySelector(".submitButton");
+  let correctAnswer;
+  submitButton.onclick = async (event) => {
+    submitButton.disabled = true;
+    correctAnswer = gradeQuestion(myQuestion); 
+    displayResult(correctAnswer);
+    const correctAnswers = correctAnswer ? 1 : 0;
+    const dataToPut = {
+      trackid: trackId,
+      test: "practicescore1",
+      score: correctAnswers,
+    };
+    const putResponse = await fetch("/api/results/", {
+      credentials: "same-origin",
+      method: "PUT",
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+      body: JSON.stringify(dataToPut),
+    });
+    const nextButton = document.querySelector(".nextButton");
+    nextButton.disabled = false;
+    nextButton.onclick = (event) => {
+      displaySecondMaterials(learningMaterials, trackId);
+    };
+  };
+}
+
+async function displaySecondMaterials(learningMaterials, trackId) {
+  clearScreen();
+  const nextButton = document.querySelector(".nextButton");
+  nextButton.disabled = true;
+  const myQuestion = await getRandomQuestion(trackId);
+  const materialsContainer = document.getElementById("materials");
+  materialsContainer.innerHTML = learningMaterials.learningtext;
+  displayQuestion(myQuestion);
+  const submitButton = document.querySelector(".submitButton");
+  let correctAnswer;
+  submitButton.onclick = async (event) => {
+    submitButton.disabled = true;
+    correctAnswer = gradeQuestion(myQuestion); 
+    displayResult(correctAnswer);
+    const correctAnswers = correctAnswer ? 1 : 0;
+    const dataToPut = {
+      trackid: trackId,
+      test: "practicescore2",
+      score: correctAnswers,
+    };
+    const putResponse = await fetch("/api/results/", {
+      credentials: "same-origin",
+      method: "PUT",
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+      body: JSON.stringify(dataToPut),
+    });
+    const nextButton = document.querySelector(".nextButton");
+    nextButton.disabled = false;
+    nextButton.onclick = (event) => {
+      displayThirdMaterials(learningMaterials, trackId);
+    };
+  };
+}
+
+async function displayThirdMaterials(learningMaterials, trackId) {
+  clearScreen();
+  const nextButton = document.querySelector(".nextButton");
+  nextButton.disabled = true;
+  const myQuestion = await getRandomQuestion(trackId);
+  const materialsContainer = document.getElementById("materials");
+  materialsContainer.innerHTML = learningMaterials.video2;
+  displayQuestion(myQuestion);
+  const submitButton = document.querySelector(".submitButton");
+  let correctAnswer;
+  submitButton.onclick = async (event) => {
+    submitButton.disabled = true;
+    correctAnswer = gradeQuestion(myQuestion); 
+    displayResult(correctAnswer);
+    const correctAnswers = correctAnswer ? 1 : 0;
+    const dataToPut = {
+      trackid: trackId,
+      test: "practicescore3",
+      score: correctAnswers,
+    };
+    const putResponse = await fetch("/api/results/", {
+      credentials: "same-origin",
+      method: "PUT",
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+      body: JSON.stringify(dataToPut),
+    });
+    const nextButton = document.querySelector(".nextButton");
+    nextButton.disabled = false;
+    nextButton.onclick = (event) => {
+      takePostTest(trackId);
+    };
+  };
+}
+
+function gradeQuestion(question) {
+  let correctAnswer = false;
+  const allAnswerSets = document.querySelectorAll(".answers");
+  for (const answerSet of allAnswerSets) {
+    const correctAnswerLetter = question.answer;
+    for (const possibleAnswer of answerSet.children) {
+      const radioButton = possibleAnswer.children[0];
+      const possibleAnswerLetter = radioButton.id;
+      if (correctAnswerLetter === possibleAnswerLetter) {
+        if (radioButton.checked) {
+          correctAnswer = true;
+          possibleAnswer.style.color = "lightgreen";
+        } else {
+          possibleAnswer.style.color = "red";
+        }
+      }
+    }
+  }
+  return correctAnswer;
+}
+
+async function takePostTest(trackId) {
+  clearScreen();
+  const subHeader = document.querySelector("h2");
+  subHeader.innerText = "Post Test";
+  const myQuestions = await getRandomQuestions(trackId);
+  // myQuestions takes the form of an array of objects, such as:
+  // { answer: "C"
+  // choicea: "Cara ordered 7 pizza for her birthday party. Her parents ate 1/2 of a pizza before the party. How much pizza is left for the party?"
+  // choiceb: "Walt has 7 hamsters. Each hamster weighs 1/2 kilogram. What is the total weight of the hamsters?"
+  // choicec: "Jenae has 1/2 kilogram of trail mix. She splits her trail mix evenly between 7 friends. How much trail mix will each friend get?"
+  // choiced: "Jack ordered 7 cupcakes for his class. His parents ate 1/2 of a cupcakes before the school. How many cupcakes are left for the class?"
+  // question: "Which problem can we solve with 1/2 ÷7?"
+  // questionid: 12
+  // trackid: 4 }
+  displayQuestions(myQuestions);
+  const submitButton = document.querySelector(".submitButton");
+  let correctAnswers = 0;
+  submitButton.onclick = async (event) => {
+    submitButton.disabled = true;
+    correctAnswers = gradeTest(myQuestions);
+    displayResults(correctAnswers);
+    const dataToPut = {
+      trackid: trackId,
+      test: "postscore",
+      score: correctAnswers,
+    };
+    const putResponse = await fetch("/api/results/", {
+      credentials: "same-origin",
+      method: "PUT",
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+      body: JSON.stringify(dataToPut),
+    });
+    const nextButton = document.querySelector(".nextButton");
+    nextButton.disabled = true;
+    nextButton.classList.add("d-none");
+  };
 }
 
 function clearScreen() {
+  const materialsDiv = document.getElementById("materials");
+  materialsDiv.innerHTML = "";
   const quizDiv = document.getElementById("quiz");
   quizDiv.innerHTML = "";
   const resultsDiv = document.getElementById("results");
@@ -95,6 +298,14 @@ async function getRandomQuestions(trackId) {
   );
   const myQuestions = await rawQuestions.json();
   return myQuestions;
+}
+
+async function getRandomQuestion(trackId) {
+  const rawQuestion = await fetch(
+    `/api?action=getquestions&track=${trackId}&num=1`
+  );
+  const myQuestion = await rawQuestion.json();
+  return myQuestion[0];
 }
 
 function displayQuestions(myQuestions) {
@@ -127,65 +338,30 @@ function displayQuestions(myQuestions) {
   nextButton.classList.remove("d-none");
 }
 
-async function takePretest(trackId) {
-  const myQuestions = await getRandomQuestions(trackId);
-  // myQuestions takes the form of an array of objects, such as:
-  // { answer: "C"
-  // choicea: "Cara ordered 7 pizza for her birthday party. Her parents ate 1/2 of a pizza before the party. How much pizza is left for the party?"
-  // choiceb: "Walt has 7 hamsters. Each hamster weighs 1/2 kilogram. What is the total weight of the hamsters?"
-  // choicec: "Jenae has 1/2 kilogram of trail mix. She splits her trail mix evenly between 7 friends. How much trail mix will each friend get?"
-  // choiced: "Jack ordered 7 cupcakes for his class. His parents ate 1/2 of a cupcakes before the school. How many cupcakes are left for the class?"
-  // question: "Which problem can we solve with 1/2 ÷7?"
-  // questionid: 12
-  // trackid: 4 }
-  displayQuestions(myQuestions);
-  const submitButton = document.querySelector(".submitButton");
-  let correctAnswers = 0;
-  submitButton.addEventListener("click", async (event) => {
-    correctAnswers = gradeTest(myQuestions);
-    displayResults(correctAnswers);
-    const dataToPut = {
-      trackid: trackId,
-      test: "pretestscore",
-      score: correctAnswers,
-    };
-    const putResponse = await fetch("/api/results/", {
-      credentials: "same-origin",
-      method: "PUT",
-      headers: new Headers({
-        "Content-Type": "application/json",
-      }),
-      body: JSON.stringify(dataToPut),
-    });
-    const nextButton = document.querySelector(".nextButton");
-    nextButton.disabled = false;
-    nextButton.addEventListener("click", (event) => {
-      displayMaterials(trackId);
-    });
-  });
-}
-
-function gradeTest(myQuestions) {
-  let numCorrect = 0;
-  let currentQuestion = 0;
-  const allAnswerSets = document.querySelectorAll(".answers");
-  for (const answerSet of allAnswerSets) {
-    const correctAnswerLetter = myQuestions[currentQuestion].answer;
-    for (const possibleAnswer of answerSet.children) {
-      const radioButton = possibleAnswer.children[0];
-      const possibleAnswerLetter = radioButton.id;
-      if (correctAnswerLetter === possibleAnswerLetter) {
-        if (radioButton.checked) {
-          numCorrect += 1;
-          possibleAnswer.style.color = "lightgreen";
-        } else {
-          possibleAnswer.style.color = "red";
-        }
-      }
-    }
-    currentQuestion += 1;
+function displayQuestion(myQuestion) {
+  // variable to store the HTML output
+  const output = [];
+  output.push("<h3>Test Your Knowledge!</h3>");
+  const answers = [];
+  for (let i = 0; i < 4; ++i) {
+    const currentAnswer = `<label>
+            <input type="radio" name="question0" id="${getChoiceLetter(
+      i
+    )}">
+            ${getChoiceLetter(i)} :
+            ${myQuestion[getChoiceName(i)]}
+          </label>`;
+    answers.push(currentAnswer);
   }
-  return numCorrect;
+  // add this question and its answers to the output
+  output.push(
+    `<div class="question"> ${myQuestion.question} </div>
+          <div class="answers"> ${answers.join("")} </div>`
+  );
+  output.push('<button class="submitButton">Submit</button');
+  // finally combine our output list into one string of HTML and put it on the page
+  const quizContainer = document.getElementById("quiz");
+  quizContainer.innerHTML = output.join("");
 }
 
 function displayResults(numCorrect) {
@@ -193,34 +369,33 @@ function displayResults(numCorrect) {
   resultsContainer.innerHTML = `<p>You got ${numCorrect} correct answers.</p>`;
 }
 
-function showResults() {
-  // gather answer containers from our quiz
+function displayResult(correctAnswer) {
+  const resultsContainer = document.getElementById("results");
+  resultsContainer.innerHTML = correctAnswer ? `<p>You got the question correct!</p>` : `<p>Sorry, wrong answer.</p>`;
+}
 
-  // keep track of user's answers
-  let numCorrect = 0;
+function getChoiceName(index) {
+  switch (index) {
+    case 0:
+      return "choicea";
+    case 1:
+      return "choiceb";
+    case 2:
+      return "choicec";
+    case 3:
+      return "choiced";
+  }
+}
 
-  // for each question...
-  myQuestions.forEach((currentQuestion, questionNumber) => {
-    // find selected answer
-    const answerContainer = answerContainers[questionNumber];
-    const selector = `input[name=question${questionNumber}]:checked`;
-    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-    // if answer is correct
-    if (userAnswer === currentQuestion.correctAnswer) {
-      // add to the number of correct answers
-      numCorrect++;
-
-      // color the answers green
-      answerContainers[questionNumber].style.color = "lightgreen";
-    }
-    // if answer is wrong or blank
-    else {
-      // color the answers red
-      answerContainers[questionNumber].style.color = "red";
-    }
-  });
-
-  // show number of correct answers out of total
-  resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+function getChoiceLetter(index) {
+  switch (index) {
+    case 0:
+      return "A";
+    case 1:
+      return "B";
+    case 2:
+      return "C";
+    case 3:
+      return "D";
+  }
 }
